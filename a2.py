e58f29db946a20dc42fd345b5d182bfeb41f915d
Sanyah Bovan <sbovan1@cps.edu>
2025-09-22 09:59:36 -0500

    idk

--- a2.py
+++ a2.py
@@ -28,19 +28,23 @@ def match(pattern: List[str], source: List[str]) -> List[str]:
         
 
         # 1) if we reached the end of the pattern but not source
-
+        if pind == len(pattern) and sind < len(source): 
+            print("end of pattern, but not source")
+            return None
         # 2) if the current thing in the pattern is a %
         # WARNING: this condition contains the bulk of the code for the assignment
         # If you get stuck on this one, we encourage you to attempt the other conditions
         #   and come back to this one afterwards
 
         # 3) if we reached the end of the source but not the pattern
-
+        elif sind == len(source):
+            print("end of source, but not pattern")
+            return None
         # 4) if the current thing in the pattern is an _
 
         # 5) if the current thing in the pattern is the same as the current thing in the
         # source
-        if pattern[pind] == source[sind]:
+        elif pattern[pind] == source[sind]:
             print(pattern[pind], source[sind])
             pind += 1
             sind += 1
